b0a9b38a57d40e9176420b6e8cf51679dcc501a5
Simon Mravec <simon.mravec@gmail.com>
2023-03-13 00:21:09 +0100

    i3, rofi, polybar

---
+++ home-manager/config/polybar.nix
@@ -0,0 +1,69 @@
+{config, pkgs, ...}:
+{
+  services.polybar.enable = true;
+  
+  nixpkgs.config = {
+    packageOverrides = pkgs: rec {
+      polybar = pkgs.polybar.override {
+        i3Support = true;
+      };
+    };
+  };
+
+  services.polybar.config = 
+  {
+  "bar/top" = {
+    monitor = "\${env:MONITOR:eDP-1}";
+    width = "100%";
+    height = "38px";
+    radius = 0;
+
+    background = "#292828";
+
+    modules-left = "launcher workspaces";
+    modules-right = "date battery";
+
+    font-0 = "JetBrains Mono:size=14;4";
+    #Logo
+    font-1 = "NotoSans Nerd Font Mono:size=23;5";
+    #Space
+    font-2 = "JetBrains Mono:size=40";
+  };
+
+  "module/launcher" = {
+    type = "custom/text";
+
+    content = "%{T1}%{T-}";
+    content-padding = 1;
+    click-left = "rofi -show drun";
+  };
+
+  "module/workspaces" = {
+     type = "custom/text";
+     
+     content = "1%{T2} %{T-}2%{T2} %{T-}3%{T2} %{T-}4";
+     content-padding = 2;
+  }; 
+
+
+  "module/date" = {
+    type = "internal/date";
+    internal = 5;
+    date = "%a %d/%m %I:%M |";
+    label = " %date% ";
+  };
+
+  "module/battery" = {
+    type = "internal/battery";
+    battery = "BAT1";
+    adapter = "ACAD";
+
+    label-charging = "%percentage%% ";
+    label-discharging = "%percentage%% ";
+    label-full = "%percentage%% ";
+    label-low = "%percentage%% ";
+  };
+  };
+
+  services.polybar.script = "polybar bar &";
+}
--- nixos/xorg/window-manager.nix
+++ nixos/xorg/window-manager.nix
@@ -1,10 +1,11 @@
 {config, pkgs, ...}:
 {
-  services.xserver.windowManager.awesome = {
+  services.xserver.windowManager.i3.package = pkgs.i3-gaps;
+  services.xserver.windowManager.i3 = {
       enable = true;
-      luaModules = with pkgs.luaPackages; [
-          luarocks
-          luadbi-mysql
-          ];
+	extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+     ];
       };
 }
