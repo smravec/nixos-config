fd41f20147298e37e640446c7782e746b16c019c
Simon Mravec <simon.mravec@gmail.com>
2023-03-14 00:30:15 +0100

    i3 config file

--- nixos/xorg/window-manager.nix
+++ nixos/xorg/window-manager.nix
@@ -1,11 +1,7 @@
 {config, pkgs, ...}:
 {
-  services.xserver.windowManager.i3.package = pkgs.i3-gaps;
   services.xserver.windowManager.i3 = {
       enable = true;
-	extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-     ];
+      configFile = "/home/simon/nixos-config/nixos/xorg/config.i3";
       };
 }
