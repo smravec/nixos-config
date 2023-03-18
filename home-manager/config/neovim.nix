07ddce7f10fae53e2c15815bbb8d821f4b03f9cb
Simon Mravec <simon.mravec@gmail.com>
2023-03-18 03:53:40 +0100

    neovim config

--- home-manager/config/neovim.nix
+++ home-manager/config/neovim.nix
@@ -37,6 +37,9 @@
         let g:NERDTreeMinimalUI = 1
         let g:NERDTreeIgnore = []
         let g:NERDTreeStatusline = ""
+        highlight NERDTreeCWD ctermfg=white
+        " Exit Vim if NERDTree is the only window remaining in the only tab.
+        autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
       '';
     }
 
@@ -61,6 +64,4 @@
     set smarttab
     set clipboard+=unnamedplus 
   '';
-
-
 }
