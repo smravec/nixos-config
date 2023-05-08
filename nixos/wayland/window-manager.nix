bdf2be8d22a02d0a30b49ae3ae7659062b9e25db
Sleroq <hireme@sleroq.link>
2023-05-08 16:30:12 +0300

    Fixed spaces

--- nixos/wayland/window-manager.nix
+++ nixos/wayland/window-manager.nix
@@ -1,8 +1,7 @@
+{ config, pkgs, lib, ... }:
 
-{config, pkgs, lib, ... }:
-
-  let
-    dbus-sway-environment = pkgs.writeTextFile {
+let
+  dbus-sway-environment = pkgs.writeTextFile {
     name = "dbus-sway-environment";
     destination = "/bin/dbus-sway-enviroment";
     executable = true;
@@ -11,52 +10,54 @@
       dbus-update-activation-enviroment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
       systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
       systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-   '';
-    };
+    '';
+  };
 
-configure-gtk = pkgs.writeTextFile {
-name = "configure-gtk";
-destination = "/bin/configure/-gtk";
-executable = true;
-text = let
-schema = pkgs.gsettings-desktop-schemas;
-datadir = "${schema}/share/gsetting-schemas/${schema.name}";
-in ''
-export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-gnome_schema=org.gnome.desktop.interface
-gsettings set $gnome_schema gtk-theme 'WhiteSur-dark'
-gsettings set $gnome_schema cursor-theme 'capitaine-cursors-white'
-'';
-};
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure/-gtk";
+    executable = true;
+    text =
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsetting-schemas/${schema.name}";
+      in
+      ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        gnome_schema=org.gnome.desktop.interface
+        gsettings set $gnome_schema gtk-theme 'WhiteSur-dark'
+        gsettings set $gnome_schema cursor-theme 'capitaine-cursors-white'
+      '';
+  };
 
 in
 {
-environment.systemPackages = with pkgs; [
-alacritty
-sway
-dbus-sway-environment
-configure-gtk
-wayland
-xdg-utils
-glib
-whitesur-icon-theme
-grim
-slurp
-wl-clipboard
-capitaine-cursors
-];
+  environment.systemPackages = with pkgs; [
+    alacritty
+    sway
+    dbus-sway-environment
+    configure-gtk
+    wayland
+    xdg-utils
+    glib
+    whitesur-icon-theme
+    grim
+    slurp
+    wl-clipboard
+    capitaine-cursors
+  ];
 
-services.dbus.enable = true;
+  services.dbus.enable = true;
 
-xdg.portal = {
-enable = true;
-wlr.enable = true;
-extraPortals = [pkgs.xdg-desktop-portal-gtk];
-};
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 
-programs.sway = {
-enable = true;
-wrapperFeatures.gtk = true;
-};
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
 
 }
